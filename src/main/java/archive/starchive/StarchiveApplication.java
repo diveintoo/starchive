7d0fc2f1363629eb0aa5f7e284f2e744f4dd0295
offsujin <offsujin@gmail.com>
2024-04-15 15:18:54 +0900

    feat: StarchiveApplication

---
+++ src/main/java/archive/starchive/StarchiveApplication.java
@@ -0,0 +1,13 @@
+package archive.starchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarchiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/archive/starchive/StarchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package archive.starchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
