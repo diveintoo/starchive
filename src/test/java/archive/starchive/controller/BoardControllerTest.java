8787a09d9dfda919d2c95180b82537196774fcb7
offsujin <offsujin@gmail.com>
2024-05-09 21:02:42 +0900

    chore: @EnableJpaAuditing 분리
    ControllerTest에서 @WebMvcTest 어노테이션과 충돌하여 JpaAuditingConfig로 분리함

--- src/test/java/archive/starchive/controller/BoardControllerTest.java
+++ src/test/java/archive/starchive/controller/BoardControllerTest.java
@@ -1,12 +1,16 @@
 package archive.starchive.controller;
 
+import archive.starchive.config.SecurityConfig;
 import archive.starchive.domain.Board;
 import archive.starchive.service.BoardService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -21,7 +25,12 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(BoardController.class)
+@WebMvcTest(
+        controllers = BoardController.class,
+        excludeAutoConfiguration = SecurityAutoConfiguration.class,
+        excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+})
 class BoardControllerTest {
 
     @Autowired
@@ -34,8 +43,8 @@ class BoardControllerTest {
     @DisplayName("GET /boards 테스트 : 성공")
     void testGetBoards() throws Exception{
         List<Board> boards = new ArrayList<>();
-        for(int i = 0; i < 7; i++) {
-            boards.add(new Board(Long.valueOf(i), "board" + Integer.toString(i)));
+        for(int i = 1; i < 8; i++) {
+            boards.add(Board.builder().id(Long.valueOf(i)).name("board" + Integer.toString(i)).build());
         }
         Page<Board> pagedResponse = new PageImpl<>(boards);
         when(boardService.findAll(any(Pageable.class))).thenReturn(pagedResponse);
@@ -43,7 +52,7 @@ class BoardControllerTest {
         this.mockMvc.perform(get("/boards"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.content[0].name").value("board0"))
-                .andExpect(jsonPath("$.content[3].id").value(3));
+                .andExpect(jsonPath("$.content[0].name").value("board1"))
+                .andExpect(jsonPath("$.content[3].id").value(4));
     }
 }
