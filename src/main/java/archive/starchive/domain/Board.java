9c6a8ad4e9c80da88ed7e381cf8535a3d7d1ec3f
offsujin <offsujin@gmail.com>
2024-05-09 20:54:11 +0900

    chore: Board Builder 추가

--- src/main/java/archive/starchive/domain/Board.java
+++ src/main/java/archive/starchive/domain/Board.java
@@ -1,12 +1,16 @@
 package archive.starchive.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
